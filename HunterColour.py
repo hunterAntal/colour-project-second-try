e6957ccaae4a8147ca9346d356f08ee48291179a
Hunter <hunterantalgithub@gmail.com>
2021-09-27 09:18:18 -0400

    add 4 colours

---
+++ HunterColour.py
@@ -0,0 +1,10 @@
+userIn = input()
+
+if userIn =='Blue':
+    print('Are you Conservitive?')
+if userIn =='Yellow':
+    print('Your pretty dumb even for a blond')
+if userIn =='Green':
+    print('If you love trees so much, why dont you marry one?')
+if userIn =='Pink':
+    print('Thats a little fruity!')
